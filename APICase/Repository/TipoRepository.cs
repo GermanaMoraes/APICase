0e31e7b007fcbf8f61ef3d1464b4a77e6feed5b4
Germana Moraes <germanamoraes95@gmail.com>
2022-09-30 17:06:00 -0300

    API Case

---
+++ APICase/Repository/TipoRepository.cs
@@ -0,0 +1,27 @@
+﻿using APICase.Data;
+using APICase.Interface;
+using APICase.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace APICase.Repository
+{
+    public class TipoRepository : ITipoUsuario
+    {
+        ClinicaContext ctx;
+
+        public TipoRepository(ClinicaContext _ctx)
+        {
+            ctx = _ctx;
+        }
+        public ICollection<TipoUsuario> GetAll()
+        {
+            return ctx.TipoUsuarios.ToList();
+        }
+
+        public TipoUsuario GetById(int id)
+        {
+            return ctx.TipoUsuarios.Find(id);
+        }
+    }
+}
---
+++ APICase/Repository/UsuarioRepository.cs
@@ -0,0 +1,56 @@
+﻿using APICase.Data;
+using APICase.Interface;
+using APICase.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace APICase.Repository
+{
+    public class UsuarioRepository : IUsuario
+    {
+        ClinicaContext ctx;
+
+        public UsuarioRepository(ClinicaContext _ctx)
+        {
+            ctx = _ctx;
+        }
+        public void Delete(Usuario usuario)
+        {
+            ctx.Usuarios.Remove(usuario);
+            ctx.SaveChanges();
+        }
+
+        public ICollection<Usuario> GetAll()
+        {
+            return ctx.Usuarios.ToList();
+        }
+
+        public Usuario GetById(int id)
+        {
+            return ctx.Usuarios.Find(id);
+        }
+
+        public Usuario Insert(Usuario usuario)
+        {
+            ctx.Usuarios.Add(usuario);
+            ctx.SaveChanges();
+            return usuario;
+        }
+
+        public void Update(Usuario usuario)
+        {
+            ctx.Entry(usuario).State = EntityState.Modified;
+            ctx.SaveChanges();
+        }
+
+        public void UpdatePatch(JsonPatchDocument patchUsuario, Usuario usuario)
+        {
+            patchUsuario.ApplyTo(usuario);
+            ctx.Entry(usuario).State = EntityState.Modified;
+            ctx.SaveChanges();
+
+        }
+    }
+}
