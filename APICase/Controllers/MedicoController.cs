09b0e48c50f031d0de596c595b488c66b0b8c055
Germana Moraes <germanamoraes95@gmail.com>
2022-09-30 17:57:07 -0300

    API Case

---
+++ APICase/Controllers/MedicoController.cs
@@ -0,0 +1,220 @@
+﻿using APICase.Interface;
+using APICase.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicoController : ControllerBase
+    {
+        private readonly IMedico repositorio;
+
+        public MedicoController(IMedico _repositorio)
+        {
+            repositorio = _repositorio;
+        }
+
+
+        /// <summary>
+        /// Cadastrar um medico no banco de dados.
+        /// </summary>
+        /// <param name="consulta"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Authorize]
+        public IActionResult Cadastrar(Medico medico)
+        {
+            try
+            {
+                var retorno = repositorio.Insert(medico);
+                return Ok(retorno);
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    Erro = "Falha na Transação",
+                    Message = ex.Message
+                });
+            }
+        }
+
+        /// <summary>
+        /// Listar todos os médicos
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                var retorno = repositorio.GetAll();
+                return Ok(retorno);
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    Erro = "Falha na Transação",
+                    Message = ex.Message
+                });
+            }
+        }
+
+        /// <summary>
+        /// Buscar um médico por Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public IActionResult BuscarMedicoporId(int id)
+        {
+            try
+            {
+                var retorno = repositorio.GetById(id);
+                if (retorno == null)
+                {
+                    return NotFound(new { Message = " Médico não encontrado." });
+                }
+
+                return Ok(retorno);
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    Erro = "Falha na Transação",
+                    Message = ex.Message
+                });
+            }
+        }
+
+
+        /// <summary>
+        /// Alterar um médico.  É necessário implementar o Id na aplicação.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="consulta"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        [Authorize]
+        public IActionResult Alterar(int id, Medico medico)
+        {
+            try
+            {
+                //Verificar se os ids batem
+                if (id != medico.Id)
+                {
+                    return BadRequest();
+                }
+
+                //Verificar se o id existe no banco
+                var retorno = repositorio.GetById(id);
+                if (retorno == null)
+                {
+                    return NotFound(new
+                    {
+                        Message = " Médico não encontrado."
+                    });
+                }
+                //Alterar
+                repositorio.Update(medico);
+                return NoContent();
+            }
+            catch (System.Exception ex)
+            {
+
+                return StatusCode(500, new
+                {
+                    Erro = "Falha na Transação",
+                    Message = ex.Message
+                });
+            }
+
+        }
+
+        /// <summary>
+        /// Alterar algo específico no Médico. Modelo: "op", "path", "value".
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="patchConsulta"></param>
+        /// <returns></returns>
+        [HttpPatch("{id}")]
+        [Authorize]
+        public IActionResult Patch(int id, [FromBody] JsonPatchDocument patchMedico)
+        {
+            try
+            {
+                if (patchMedico == null)
+                { return BadRequest(); }
+
+                var medico = repositorio.GetById(id);
+                if (medico == null)
+                {
+                    return NotFound(new { Message = "Consulta não encontrada." });
+                }
+                repositorio.UpdatePatch(patchMedico, medico);
+
+
+                return Ok(medico);
+
+
+            }
+            catch (System.Exception ex)
+            {
+
+                return StatusCode(500, new
+                {
+                    Erro = "Falha na Transação",
+                    Message = ex.Message
+                });
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// Deletar uma Consulta.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        [Authorize]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var item = repositorio.GetById(id);
+                if (item == null)
+                {
+                    return NotFound();
+                }
+                repositorio.Delete(item);
+                return NoContent();
+
+
+
+            }
+            catch (System.Exception ex)
+            {
+
+                return StatusCode(500, new
+                {
+                    Erro = "Falha na Transação",
+                    Message = ex.Message
+                });
+            }
+        }
+
+
+
+    }
+
+}
+
+
---
+++ APICase/Controllers/PacienteController.cs
@@ -0,0 +1,217 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using APICase.Interface;
+using APICase.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace APICase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PacienteController : ControllerBase
+    {
+
+        private readonly IPaciente repositorio;
+
+        public PacienteController(IPaciente _repositorio)
+        {
+            repositorio = _repositorio;
+        }
+
+
+        /// <summary>
+        /// Cadastrar um paciente no banco de dados.
+        /// </summary>
+        /// <param name="paciente"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Authorize]
+        public IActionResult Cadastrar(Paciente paciente)
+        {
+            try
+            {
+                var retorno = repositorio.Insert(paciente);
+                return Ok(retorno);
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    Erro = "Falha na Transação",
+                    Message = ex.Message
+                });
+            }
+        }
+
+        /// <summary>
+        /// Listar todos os pacientes.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                var retorno = repositorio.GetAll();
+                return Ok(retorno);
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    Erro = "Falha na Transação",
+                    Message = ex.Message
+                });
+            }
+        }
+
+        /// <summary>
+        /// Buscar um paciente por Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public IActionResult BuscarPacienteporId(int id)
+        {
+            try
+            {
+                var retorno = repositorio.GetById(id);
+                if (retorno == null)
+                {
+                    return NotFound(new { Message = " Paciente não encontrado." });
+                }
+
+                return Ok(retorno);
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    Erro = "Falha na Transação",
+                    Message = ex.Message
+                });
+            }
+        }
+
+
+        /// <summary>
+        /// Alterar um paciente.  É necessário implementar o Id na aplicação.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="medico"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        [Authorize]
+        public IActionResult Alterar(int id, Paciente paciente)
+        {
+            try
+            {
+                //Verificar se os ids batem
+                if (id != paciente.Id)
+                {
+                    return BadRequest();
+                }
+
+                //Verificar se o id existe no banco
+                var retorno = repositorio.GetById(id);
+                if (retorno == null)
+                {
+                    return NotFound(new
+                    {
+                        Message = " Paciente não encontrado."
+                    });
+                }
+                //Alterar
+                repositorio.Update(paciente);
+                return NoContent();
+            }
+            catch (System.Exception ex)
+            {
+
+                return StatusCode(500, new
+                {
+                    Erro = "Falha na Transação",
+                    Message = ex.Message
+                });
+            }
+
+        }
+
+        /// <summary>
+        /// Alterar algo específico no paciente. Modelo: "op", "path", "value".
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="patchMedico"></param>
+        /// <returns></returns>
+        [HttpPatch("{id}")]
+        [Authorize]
+        public IActionResult Patch(int id, [FromBody] JsonPatchDocument patch)
+        {
+            try
+            {
+                if (patch == null)
+                { return BadRequest(); }
+
+                var paciente = repositorio.GetById(id);
+                if (paciente == null)
+                {
+                    return NotFound(new { Message = "Paciente não encontrada." });
+                }
+                repositorio.UpdatePatch(patch, paciente);
+
+
+                return Ok(paciente);
+
+
+            }
+            catch (System.Exception ex)
+            {
+
+                return StatusCode(500, new
+                {
+                    Erro = "Falha na Transação",
+                    Message = ex.Message
+                });
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// Deletar um Paciente.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        [Authorize]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var item = repositorio.GetById(id);
+                if (item == null)
+                {
+                    return NotFound();
+                }
+                repositorio.Delete(item);
+                return NoContent();
+
+
+
+            }
+            catch (System.Exception ex)
+            {
+
+                return StatusCode(500, new
+                {
+                    Erro = "Falha na Transação",
+                    Message = ex.Message
+                });
+            }
+        }
+
+
+    }
+}
