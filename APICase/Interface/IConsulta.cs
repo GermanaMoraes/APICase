46c625d280c9307851153ddab05644a604eca393
Germana Moraes <germanamoraes95@gmail.com>
2022-09-30 13:20:34 -0300

    Adicionar arquivos de projeto.

---
+++ APICase/Interface/IConsulta.cs
@@ -0,0 +1,28 @@
+﻿using APICase.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using System.Collections.Generic;
+
+namespace APICase.Interface
+{
+    public interface IConsulta
+    {
+
+        //Inserir Consulta
+        Consulta Insert(Consulta consulta);
+
+        //Listar todas as consultas
+        ICollection<Consulta> GetAll();
+
+        //Consultar por Id
+        Consulta GetById(int id);
+
+        //Alterar a Consulta
+        void Update(Consulta consulta);
+
+        //Deletar a Consulta
+        void Delete(int id);
+
+        //Update com Patch
+        void UpdatePatch(JsonPatchDocument patchconsulta, Consulta consulta);
+    }
+}
---
+++ APICase/Models/Paciente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace APICase.Models
+{
+    public partial class Paciente
+    {
+        public Paciente()
+        {
+            Consulta = new HashSet<Consulta>();
+        }
+
+        public int Id { get; set; }
+        public string Carterinha { get; set; }
+        public DateTime? DataNascimento { get; set; }
+        public bool? Ativo { get; set; }
+        public int? IdUsuario { get; set; }
+
+        public virtual Usuario IdUsuarioNavigation { get; set; }
+        public virtual ICollection<Consulta> Consulta { get; set; }
+    }
+}
